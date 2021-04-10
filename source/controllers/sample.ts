70a1c73609e6893597bdb87eea7c84ed53526b0e
mike <y6602016@gmail.com>
2021-04-10 20:27:49 +0800

    update

--- source/controllers/sample.ts
+++ source/controllers/sample.ts
@@ -3,7 +3,7 @@ import logging from '../config/logging';
 
 const NAMESPACE = 'Sample Controller';
 
-const smapleHealthCheck = (req: Request, res: Response, next: NextFunction) => {
+const smapleHealthCheck = (req: Request, res: Response, next: NextFunction): {} => {
     logging.info(NAMESPACE, `Sample health check route called.`);
 
     return res.status(200).json({
